6a5512b7aac73698c4cc290a753a52a7d75e64a2
KKBIRD23 <4355089@qq.com>
2022-04-19 23:41:28 +0800

    1. 迭代遍历的使用； 2. 名片管理系统

---
+++ Demos/learning/05_高级变量/ln_16_遍历字典的列表.py
@@ -0,0 +1,18 @@
+students = [
+    {"name": "阿土",
+     "age": "19"},
+    {"name": "小美",
+     "age": "18"}
+]
+find_name = "小美"
+find_age = "19"
+for stu_dict in students:
+    print(stu_dict)
+    if stu_dict["age"] == find_age:
+        print(f'找到了 {stu_dict["name"]} , 年龄是{stu_dict["age"]}')
+        # 找到就退出
+        break
+else:
+    # 如果遍历完成都没有找到搜索目标，执行以下
+    print(f"抱歉没有找到 {find_age}")
+print("循环结束")
