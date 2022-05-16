06930be30c0e21be7a983fbd057dafb9dcf2b55b
道格拉斯.老王 <4355089@qq.com>
2022-05-16 13:20:21 +0800

    修正

--- Demos/learning/11_设计模式/ln_01_单例设计模式.py
+++ Demos/learning/11_设计模式/ln_01_单例设计模式.py
@@ -3,5 +3,26 @@
 单例设计模式：即单实例设计模式。应用场景如：音乐播放器
     - 目的 —— 让 类 创建的对象，在系统中 只有 唯一的一个实例
     - 每一次执行 类名() 返回的对象，内存地址是相同的
-
+----------------------------------------------------------------------------
+__new__方法的重写格式是固定的：
+def __new__(cls, *args, **kwargs):
+    新代码
+    return super().__new__(cls)
 """
+
+
+class MusicPlayer(object):
+
+    def __new__(cls, *args, **kwargs):
+        # 1. 创建对象时，new方法会被自动调用
+        print("创建对象，分配空间")
+        # 2. 为对象分配空间,返回对象的引用
+        return super().__new__(cls)
+
+    def __init__(self):
+        print("播放器初始化")
+
+
+player = MusicPlayer()
+
+print(player)
