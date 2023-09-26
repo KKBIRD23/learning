8835c5efd4afe3c2c4fa9772658b02ab145355e8
KKBIRD23 <4355089@qq.com>
2023-09-27 03:14:21 +0800

    1

--- Demos/VFJReaderTool/TestDLL_cxFreeze/setup.py
+++ Demos/VFJReaderTool/TestDLL_cxFreeze/setup.py
@@ -9,13 +9,17 @@ application_version = "1.0"
 executables = [Executable('VFJ读写器综合工具.py', base=None, target_name = 'VFJReaderTool')]
 
 # 包含的文件夹及其目标路径（在可执行文件目录下）
-include_files = [("DLL", "DLL")]
+include_files = [("DLL", "DLL"),("rapicomm.ini","rapicomm.ini")]
 
 # 定义应用程序的依赖项
 includes = ["VFJReader"]
 excludes = []
 packages = []
-build_exe_options = {"includes": includes, "excludes": excludes, "packages": packages}
+build_exe_options = {
+    "includes": includes,
+    "excludes": excludes,
+    "packages": packages
+}
 
 # 创建 setup
 setup(
